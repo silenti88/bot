8136a3f400dd2a035280f323a68268a34257d7cc
Dominic Humber <dhumber2003@gmail.com>
2019-04-02 23:26:34 -0400

    Set up
    Moved around the files

--- disconnect.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = client => {
-        // Runs when bot goes offline
-        console.log(`You have been disconnected`)
-}
--- error.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = client => {
-    // Runs when there is an error
-    console.error();
-}
